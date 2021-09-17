1055875f2492ec2dc60df426d5e8bda708fe4c22
Diloma Ouattara <dilomaouattara7@gmail.com>
2021-09-17 15:29:05 +0200

    Pushing the project to my Git repository

---
+++ Script.js
@@ -0,0 +1,17 @@
+$(document).ready(function () {
+  $('#sel').change(function () {
+    $(this).find("option:selected").each(function () {
+      var optionValue = $(this).attr("value");
+      if (optionValue === 'Buy') {
+        $('#Buy_prop').show();
+        $('#Rental').hide();
+      } else if (optionValue === 'Renting') {
+        $('#Rental').show();
+        $('#Buy_prop').hide();
+      } else {
+        $('#Buy_prop').hide();
+        $('#Rental').hide();
+      }
+    });
+  }).change();
+});
